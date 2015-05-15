7786c64da32650a0b7e47b769498e7250ac29464
drocta <madaco.madaco@gmail.com>
2015-05-14 21:09:08 -0700

    Add mko() for monitoring while mining.
    mko() displays the coinbaseBalance() as well as the mineTimeEstimate(), and ALSO keeps track of the hashrate and the most recent block number.
    If either of these are exactly the same as they were the last time mko() was called, it suggests that there might be a problem, so it gives the user an error warning them. Neither of these imply that there necessarily is a problem, but they might suggest that one might want to check on things.
    The function is called mko because it relates to mining, so it starts with m, and the keys are right in a row on the way to the parens.

--- js/utils.js
+++ js/utils.js
@@ -2,6 +2,13 @@
 
 loadScript('js/balances.js');//loads balances.js in case it isn't already loaded
 
+if(typeof(utilvs)=='undefined'){
+utilvs={}//object with some variables for these functions. Put in an object to avoid name collisions.
+utilvs.lastHashrate=0
+utilvs.blockNumber=0
+}
+
+
 //asdf() is meant to give a basic concise overview of what's happening. Named asdf for fast typing
 //(e.g. in case one's typing is lagging a little bit because one is mining, the function name requires few keystrokes)
 function asdf(){
@@ -16,6 +23,25 @@ function mineTimeEstimate(){//outputs to console.log estimated time to mine in H
 	adjustedHashRate=admin.miner.hashrate()*1000*1.04;//1000 is to fix an error. 1.08 is assuming at least 2 uncles every 25 blocks.
 	//Remove the factor of 1000 once this issue has been fixed in the version of geth which you are using.
 	estimatedSeconds=eth.getBlock("latest").difficulty/adjustedHashRate;
-	console.log(Math.floor(estimatedSeconds/3600.)+" Hrs, "+Math.floor((estimatedSeconds%3600)/60)+" Mins, "+Math.floor(estimatedSeconds%60)+" Sec, estimated.");
+	console.log(Math.floor(estimatedSeconds/3600.)+" Hrs, "+Math.floor((estimatedSeconds%3600)/60)+" Mins, "+Math.floor(estimatedSeconds%60)+" Sec, estimated time until next successful block mined.");
 	return estimatedSeconds;
+}
+
+function mko(){
+	mkohashrate=admin.miner.hashrate();
+	if(mkohashrate==0){
+		console.log("warning: hashrate is (still?) zero.");
+		utilvs.lastHashRate=0;
+		//return;
+	}
+	if(mkohashrate==utilvs.lastHashrate){
+		console.log("warning: hashrate is still "+mkohashrate+"kH/s");
+	}
+	if(eth.blockNumber==utilvs.blockNumber){
+		console.log("warning: blocknumber is still "+eth.blockNumber+".");
+	}
+	utilvs.lastHashrate=mkohashrate;
+	utilvs.blockNumber=eth.blockNumber;
+	mineTimeEstimate();
+	console.log(coinbaseBalance());
 }
