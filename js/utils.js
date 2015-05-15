ade0c6dc1ae00350f9c3fdc8f0d3b910039977d0
drocta <madaco.madaco@gmail.com>
2015-05-14 20:05:36 -0700

    Add mineTimeEstimate()
    mineTimeEstimate calculates expected time until next block successfully mined, including uncles. Note that this takes into account geth currently misreporting the hashrate by a factor of 1000. When that gets fixed, this will have to be updated (by pulling out the 1000* )
    This assumes also that there are at least 2 uncles included every 25 blocks, on average. This does not actually change the prediction by much, but if uncles become particularly common, (say, an average of 1 per block) then the 1.04 would be changed to reflect this (e.g. to 2.0)

--- js/utils.js
+++ js/utils.js
@@ -10,3 +10,12 @@ function asdf(){
 	console.log("block#  :"+eth.blockNumber);
 	console.log("peers   :"+net.peerCount);
 }
+
+//this might later go in a file specifically for mining.
+function mineTimeEstimate(){//outputs to console.log estimated time to mine in Hours Mins Seconds. Returns estimted time in seconds.
+	adjustedHashRate=admin.miner.hashrate()*1000*1.04;//1000 is to fix an error. 1.08 is assuming at least 2 uncles every 25 blocks.
+	//Remove the factor of 1000 once this issue has been fixed in the version of geth which you are using.
+	estimatedSeconds=eth.getBlock("latest").difficulty/adjustedHashRate;
+	console.log(Math.floor(estimatedSeconds/3600.)+" Hrs, "+Math.floor((estimatedSeconds%3600)/60)+" Mins, "+Math.floor(estimatedSeconds%60)+" Sec, estimated.");
+	return estimatedSeconds;
+}
